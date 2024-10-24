690c6f6ee910420c99f29a45c9ea0e35b5c0dc81
Élcio Reis <elcio.reis024@gmail.com>
2024-10-24 17:35:06 -0300

    Adicionado msg ao javascript do button

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,9 @@
 
 document.querySelectorAll(".contato").forEach((item) => {
     item.addEventListener("click", (event) => {
-        window.open("https://api.whatsapp.com/send/?phone=5599982219604&text&type=phone_number&app_absent=0")
+        const numero = 5599982219604;
+        const msg = 'Olá, gostaria de saber mais informações.';
+        const url = `https://api.whatsapp.com/send?phone=${numero}&text=${encodeURIComponent(msg)}`;
+        window.open(url);
     })
 })
