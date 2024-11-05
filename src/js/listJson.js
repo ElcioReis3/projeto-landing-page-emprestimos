6f089f730266f1228814017d517dc3959df69235
Élcio Reis <elcio.reis024@gmail.com>
2024-11-05 18:38:38 -0300

    Adicionado listjson

---
+++ src/js/listJson.js
@@ -0,0 +1,14 @@
+const listJson = [
+    {
+        title: 'Quem tem direito ao empréstimo consignado?',
+        description: 'O empréstimo consignado pode ser solicitado por aposentados, pensionistas do INSS, servidores públicos, militares das forças armadas(conforme convênio) e trabalhadores com carteira assinada, vinculado à empresa que trabalha(consignado privado).',
+    },
+    {
+        title: 'O que é margem consignável?',
+        description: 'A margem consignável é estabelecida por lei e define o limite máximo de comprometimento do seu salário/ benefício para pagamento das parcelas de um empréstimo consignado.Atualmente, a margem consignável do INSS é de 40 %, sendo que até 35 % do recebimento mensal pode ser descontado para o pagamento das parcelas do empréstimo consignado, e 5 % para o pagamento do cartão de crédito consignado.',
+    },
+    {
+        title: 'Preciso verificar o andamento do empréstimo de um parente, é possível?',
+        description: 'Em respeito à lei geral de proteção de dados, do sigilo bancário e segurança dos nossos clientes, só passamos informação ao titular.Você precisa pedir para o titular do empréstimo entrar em contato com nossos especialistas.',
+    }
+]
--- src/js/script.js
+++ src/js/script.js
@@ -9,9 +9,21 @@ document.querySelectorAll(".contato").forEach((item) => {
     })
 })
 
-document.querySelectorAll('.infoCard').forEach((item) => {
-    item.addEventListener('click', (event) => {
-        let card = item.children[1]
+
+listJson.map(item => {
+    let { title, description } = item;
+    const newCard = document.querySelector('.infoCard').cloneNode(true);
+
+    newCard.style.display = 'inline';
+    newCard.querySelector('h2').innerHTML = title;
+    newCard.querySelector('.cardHiddem').innerHTML = description;
+
+    document.querySelector('.information').append(newCard);
+
+})
+document.querySelectorAll('.infoCard').forEach((itens) => {
+    itens.addEventListener('click', (event) => {
+        let card = itens.children[1]
         card.classList.toggle('showAbout');
     })
 })
